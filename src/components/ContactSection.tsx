43ccb4cd5fd56ba654bf30b2ddbf45ad37bee5bb
Ketan Shukla <ketankshukla@gmail.com>
2025-04-10 19:38:05 -0700

    updated email address

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -20,8 +20,8 @@ const ContactSection: FC = () => {
                 <svg className="w-6 h-6 text-[#FFD43B]" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"></path>
                 </svg>
-                <a href="mailto:resume@ketanshukla.com" className="text-[#FFD43B] hover:underline">
-                  resume@ketanshukla.com
+                <a href="mailto:resume@ketankshukla.com" className="text-[#FFD43B] hover:underline">
+                  resume@ketankshukla.com
                 </a>
               </div>
               <div className="flex items-center gap-4">
